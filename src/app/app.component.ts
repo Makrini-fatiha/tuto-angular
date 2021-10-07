8735455d776375385cf3df872f315c8e046f343c
unknown <makrinifatiha1997@gmail.com>
2021-10-07 14:37:13 +0100

    modification tourof heroes

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+  cercle = true;
+
+  changeColor() {
+    this.cercle=!this.cercle
+  }
+}
+
